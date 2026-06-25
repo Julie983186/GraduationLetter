f77108b6059915cabdee01eab03edad4dbae035c
Julie <dipjuolty883@gmail.com>
2026-06-25 23:18:09 +0800

    BGM4

--- src/components/BGM.jsx
+++ src/components/BGM.jsx
@@ -1,9 +1,33 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 export default function BGM() {
+  const audioRef = useRef(null);
+
+  const startMusic = async () => {
+    try {
+      await audioRef.current.play();
+    } catch (err) {
+      console.log("play blocked:", err);
+    }
+  };
+
   return (
-    <audio autoPlay loop>
-      <source src="/Wildfire.mp3" type="audio/mpeg" />
-    </audio>
+    <>
+      <button
+        onClick={startMusic}
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          zIndex: 9999
+        }}
+      >
+        🎵 Play Music
+      </button>
+
+      <audio ref={audioRef} loop>
+        <source src="/Wildfire.mp3" type="audio/mpeg" />
+      </audio>
+    </>
   );
 }
